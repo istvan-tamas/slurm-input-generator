e97f622bd506447239cae7dc99d2a6141728d2a7
istvan-tamas <istvan.tamas90@gmail.com>
2023-01-08 18:59:15 +0100

    Initial commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+function ExpenseItem() {
+    return (
+      <div>
+        <h1>This is something different</h1>
+        <ul>
+            <li>
+                This is something different
+            </li>
+
+        </ul>
+      </div>
+    );
+  }
+  
+  export default ExpenseItem;
