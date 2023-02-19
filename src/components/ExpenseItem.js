9f76f495fd09e275846607abd7c295ed859b42c4
istvan-tamas <istvan.tamas90@gmail.com>
2023-02-19 12:14:43 +0100

    Fixed a bug in the ExpenseItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -9,8 +9,9 @@ function ExpenseItem(props) {
         <ExpenseDate date={props.date}></ExpenseDate>
         <div className='expense-item__description'>
           <h2>{props.title}</h2>
+          <div className='expense-item__price'>${props.amount}</div>
         </div>
-          <div className='expense-item__price'>${props.amount}</div>     
+          <button>Change Title</button>  
       </Card>
     );
   }
