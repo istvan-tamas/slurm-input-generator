b3ff37d0ac6d651ee4fb3921f453f86dcaddfbbd
istvan-tamas <istvan.tamas90@gmail.com>
2023-02-20 15:15:09 +0100

    Fixed a CSS bug with the Card component

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -3,7 +3,6 @@ import Card from "./Card";
 import './Expenses.css';
 
 function Expenses(props){
-    const classes = 'card ' + props.className;
     return <Card className="expenses">
         <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItem>
         <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}></ExpenseItem>
