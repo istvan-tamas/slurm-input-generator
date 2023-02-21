277cf5a86f1dfdc8b2dc56cdec41dfb472690acc
istvan-tamas <istvan.tamas90@gmail.com>
2023-02-21 21:30:46 +0100

    Minor changes in the state

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -8,7 +8,7 @@ function ExpenseItem(props) {
     const [title, setTitle] = useState(props.title);
 
     const clickHandler = () => {
-      setTitle("Updated");
+      setTitle("Evaluated by React");
     }
 
     return (
