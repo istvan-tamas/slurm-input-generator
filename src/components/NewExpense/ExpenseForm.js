604b7cf2c055714790d24d2f6965dcb36efd1b34
istvan-tamas <istvan.tamas90@gmail.com>
2023-02-27 12:28:07 +0100

    Added handler functions and states

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,17 +3,19 @@ import React, { useState } from 'react';
 
 const ExpenseForm = () => {
     const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
 
     const titleChangeHandler = (event) => {
            setEnteredTitle(event.target.value);
     };
 
     const amountChangeHandler = (event) =>{
-
+            setEnteredAmount(event.target.value);
     };
 
     const dateChangeHandler = (event) =>{
-        
+            setEnteredDate(event.target.value);
     }
 
     return(
