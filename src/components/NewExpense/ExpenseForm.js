ca6f2ab56c3ec4cbde557b8e32a75baa96028b8b
istvan-tamas <istvan.tamas90@gmail.com>
2023-02-27 12:22:56 +0100

    Added event handlers for the amount and the date

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,10 +1,21 @@
-import React from "react";
 import './ExpenseForm.css';
+import React, { useState } from 'react';
 
 const ExpenseForm = () => {
+    const [enteredTitle, setEnteredTitle] = useState('');
+
     const titleChangeHandler = (event) => {
-            console.log(event);
+           setEnteredTitle(event.target.value);
     };
+
+    const amountChangeHandler = (event) =>{
+
+    };
+
+    const dateChangeHandler = (event) =>{
+        
+    }
+
     return(
         <form>
             <div className="new-expense__controls">
@@ -14,11 +25,11 @@ const ExpenseForm = () => {
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01"/>          
+                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>          
                 </div>   
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" min="2023-01-01" max="2023-12-31"/>             
+                    <input type="date" min="2023-01-01" max="2023-12-31" onChange={dateChangeHandler}/>             
                 </div>   
             </div>
             <div className="new_expense__actions">
