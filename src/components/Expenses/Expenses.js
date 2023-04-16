4117d71a33c566111c64bb6fac1442cf31eba71f
istvan-tamas <istvan.tamas90@gmail.com>
2023-04-16 19:22:52 +0200

    Added filtering by date

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -9,7 +9,13 @@ function Expenses(props) {
 
 	const filterChangeHandler = (selectedYear) => {
 		setFilteredYear(selectedYear);
+		console.log('changed');
 	};
+
+	const filteredExpenses = props.items.filter((expense) => {
+		return expense.date.getFullYear().toString() === filteredYear;
+	});
+
 	return (
 		<div>
 			<Card className="expenses">
@@ -17,7 +23,7 @@ function Expenses(props) {
 					selected={filteredYear}
 					onChangeFilter={filterChangeHandler}
 				/>
-				{props.items.map((expense) => (
+				{filteredExpenses.map((expense) => (
 					<ExpenseItem
 						key={expense.id}
 						title={expense.title}
